634239ce23e1655c0ac9ad804243cc9576fbb91d
Amir Abu Shareb <yields@icloud.com>
2014-04-12 03:20:43 +0300

    add debug()

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 
 /**
+ * Module dependencies.
+ */
+
+var debug = require('debug');
+
+/**
  * Expose `generate`.
  */
 
 module.exports = generate;
-
 
 /**
  * Generate a global queue pushing method with `name`.
@@ -16,11 +21,13 @@ module.exports = generate;
  */
 
 function generate (name, options) {
+  var log = debug('global-queue:' + name);
   options = options || {};
 
   return function (args) {
     args = [].slice.call(arguments);
     window[name] || (window[name] = []);
+    log('%o', args);
     options.wrap === false
       ? window[name].push.apply(window[name], args)
       : window[name].push(args);
